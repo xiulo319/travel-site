53de783b23d1785287bccc548a43259d22703fd5
xiulo319 <junyikloo8@gmail.com>
2025-05-13 16:45:42 +0800

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,5 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://zdmdrwndtjbrfppnxiuu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbWRyd25kdGpicmZwcG54aXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMTcxNTgsImV4cCI6MjA2MjY5MzE1OH0.nZr3Pc7dodUJwiiV2dqb03mdVuNGwzkpkcHbACePIFw';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://zdmdrwndtjbrfppnxiuu.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
